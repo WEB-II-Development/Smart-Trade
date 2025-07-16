b7b6a899b13dfdf3bacc01472ddc3f20aa66120b
Sanjana Nisal Kumara <sanjanakumara92@gmail.com>
2025-07-16 11:52:07 +0530

    store user session detail save to user object

--- SmartTrader/src/java/controller/VerifyAccount.java
+++ SmartTrader/src/java/controller/VerifyAccount.java
@@ -28,68 +28,72 @@ import org.hibernate.criterion.Restrictions;
  */
 @WebServlet(name = "VerifyAccount", urlPatterns = {"/VerifyAccount"})
 public class VerifyAccount extends HttpServlet {
-    
+
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        
+
         Gson gson = new Gson();
-        
+
         JsonObject responseObject = new JsonObject();
         responseObject.addProperty("status", false);
 
         // first step is chechk email save in saession
         HttpSession ses = request.getSession();
-        
+
         if (ses.getAttribute("email") == null) {
-            
+
 //            responseObject.addProperty("message", "Email not found!!");
             responseObject.addProperty("message", "1");
-            
+
         } else {
-            
+
             String email = ses.getAttribute("email").toString();
-            
+
             JsonObject verification = gson.fromJson(request.getReader(), JsonObject.class);
-            
+
             String verificationCode = verification.get("verificationCode").getAsString();
 
 //            System.out.println(verificationCode);
             SessionFactory sf = HibernateUtil.getSessionFactory();
             Session s = sf.openSession();
-            
+
             Criteria c1 = s.createCriteria(User.class);
-            
+
             Criterion crt1 = Restrictions.eq("email", email);
             Criterion crt2 = Restrictions.eq("verification", verificationCode);
-            
+
             c1.add(crt1);
             c1.add(crt2);
-            
+
             if (c1.list().isEmpty()) {
-                
+
                 responseObject.addProperty("message", "Invalid Verification code!!");
-                
+
             } else {
-                
+
                 User user = (User) c1.list().get(0);
                 user.setVerification("Verifide");
-                
+
                 s.update(user);
                 s.beginTransaction().commit();
                 s.close();
-                
+
+                // store user in the session
+                ses.setAttribute("user", user);
+                // store user in the session
+
                 responseObject.addProperty("status", true);
                 responseObject.addProperty("message", "Verifide successful!");
-                
+
             }
-            
+
         }
-        
+
         String responseText = gson.toJson(responseObject);
         response.setContentType("application/json");
         response.getWriter().write(responseText);
-        
+
     }
-    
+
 }
