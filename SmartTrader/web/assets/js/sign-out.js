bae70bfe4ae336be9070eb5fab2d5b1f02d7560c
Sanjana Nisal Kumara <sanjanakumara92@gmail.com>
2025-07-17 10:13:03 +0530

    Complete Sign Out Process

---
+++ SmartTrader/web/assets/js/sign-out.js
@@ -0,0 +1,26 @@
+async function SignOut() {
+
+    const response = await fetch("SignOut");
+    
+    if (response.ok) {
+        
+        const json = await response.json();
+
+        if (json.status) {
+            
+            window.location = "sign-in.html";
+            
+        } else {
+            
+            window.location.reload();
+            
+        }
+        
+    } else {
+        
+        console.log("LogOut Failed");
+        
+
+    }
+
+}
